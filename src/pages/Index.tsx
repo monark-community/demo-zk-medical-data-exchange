a87f47157ac4f047eea8b2ca9b1c0296d0a44568
Dominic Fournier <me@dominicfournier.com>
2025-09-12 12:19:20 -0400

    Added demo project.

---
+++ src/pages/Index.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+import FeaturesSection from '../components/FeaturesSection';
+import StatsSection from '../components/StatsSection';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <HeroSection />
+      <FeaturesSection />
+      <StatsSection />
+    </div>
+  );
+};
+
+export default Index;
